41cb5890c64fa2cdee071c745f0bd6b6ac52ec56
heyijun <heyijun@sensesw.com>
2024-11-20 21:36:00 +0800

    add url

--- v8/spnego/http.go
+++ v8/spnego/http.go
@@ -268,7 +268,7 @@ func SPNEGOKRB5Authenticate(inner http.Handler, kt *keytab.Keytab, settings ...f
 			return
 		}
 
-		// get st
+		// get st from Authorization header
 		st, err := getAuthorizationNegotiationHeaderAsSPNEGOToken(spnego, r, w)
 		if st == nil || err != nil {
 			// response to client and logging handled in function above so just return
